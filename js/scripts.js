b919ec7e6ad64e4f2959b915acf45b90e7815298
Thomas Preece <thomas@thomaspreece.net>
2016-07-26 10:39:32 +0100

    Fade and slide

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,7 @@ $(function() {
 	});
 	
 	var animateScroll = function() {
+		// fade top bar
 		var currentpos = $(window).scrollTop();
 		var topbar = $('.topbar').height();
 		var windowheight = window.innerHeight;
@@ -36,20 +37,24 @@ $(function() {
 		}		
 		$('.topbar .background').css('opacity', opacity);
 		
+		// fade and slide title		
 		var title = $('.topbar h1').height();
-		
 		var top = (topbar+0.0) / 2.0 - (title+0.0) / 2.2;
 		var start = (windowheight+0.0) / 2.0;
 		var distance = (topbar+0.0);
+		opacity = 1;
 		
 		if (currentpos < start) {
 			top = -title;
+			opacity = 0;
 		} else if (currentpos < (start + distance)) {
 			var frac = (currentpos - start + 0.0) / (distance + 0.0);
 			top = -title + (title + top + 0.0) * frac;
+			opacity = frac;
 		}
-		$('.topbar h1').css('top', top);
+		$('.topbar h1').css('top', top).css('opacity', opacity);
 		
+		// update top bar links
 		if (scroll_links) {
 			var name = 'home';
 			var highestpos = 0;
